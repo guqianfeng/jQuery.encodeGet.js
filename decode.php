89d8432bea6f31ee42a8ccc8b511730f7f16d53f
Jacky <jackygu2006@163.com>
2013-09-19 21:23:43 +0800

    Create decode.php

---
+++ decode.php
@@ -0,0 +1,23 @@
+<?php
+require_once( "include/common.inc.php" );
+require_once( "include/ppd.func.php" );
+require_once( "include/ppd.encodeget.php" );
+
+echo '<p>decoded $_GET</p>';
+print_r( $_GET );
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>GET ENCODE</title>
+<style>
+*{font-size:14px;font-family:Verdana, Geneva, sans-serif;}
+a{color:#2D7DF7;}
+</style>
+</head>
+
+<body>
+</body>
+</html>
